5489dab207aa4a951b4ebae1eb44441a6802e781
Steven <steven2358@gmail.com>
2013-09-27 19:44:55 +0200

    cleaning up

--- simple_slide.js
+++ simple_slide.js
@@ -1,25 +1,23 @@
 // https://github.com/steven2358/simple_slide
 
 function show_next(ul_id, fadetime) {
-	$active_slide = $("#"+ul_id+" li.active");
+	$active_slide = $(ul_id).children(".active");
 	$next_slide = $active_slide.next();
 	if ($next_slide.index()<0) {
-		$next_slide = $("#"+ul_id+" li:first");
+		$next_slide = $(ul_id).children().first();
 	}
 	
-	$active_slide.removeClass('active').addClass('hiding');
+	$active_slide.removeClass('active');
 	$active_slide.animate({opacity: 0}, fadetime);
 	
 	$next_slide.show().css({opacity: 0})
 	.addClass('active')
-	.animate({opacity: 1}, fadetime, function() {
-		$active_slide.removeClass('hiding');
-		});
+	.animate({opacity: 1}, fadetime);
 }
 
 function simple_slide(ul_id, duration, fadetime){	
-	$("#"+ul_id+" li").css({opacity: 0});
-	$("#"+ul_id+" li:first").show().addClass("active").css({opacity: 1});
+	$(ul_id).children().css({opacity: 0}).removeClass("active");
+	$(ul_id).children().first().show().addClass("active").css({opacity: 1});
 	
 	// rotate all images
 	play = setInterval(function(){
